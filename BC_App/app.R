c46330bd76d4409fcb6aa3845c07189735f2230b
aicon08 <aira.contreras@gmail.com>
2019-04-21 19:02:23 +0000

    Development of a small shiny app using the Iris dataset--contains
    -Summary Tab
    -Species Input Tab
    -Basic Histogram Plot
    -Complex Histogram Plot

--- BC_App/app.R
+++ BC_App/app.R
@@ -5,30 +5,57 @@ library(dplyr)
 ui <- fluidPage(
 
     # Application title
-    titlePanel("Rock, Paper, Scissors"),
+    titlePanel(title = h4("Exploration of the Iris Dataset--Learning to Use Shiny",align="center")),
 
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
-        sidebarPanel(
-            actionButton("action1",label = "Rock"),
-            actionButton("action2",label = "Paper"),
-            actionButton("action3", label="Scissors")
-        ),
+            sidebarPanel(
+                selectInput("species","Select the Species",
+                            choices = list("setosa","versicolor","virginica"),selected = "virginica"),
+                selectInput("var","Select the data Input",
+                            choices = list("Sepal.Length" = 1,"Sepal.Width" = 2,"Petal.Length" = 3,"Petal.Width" = 4),
+                            selected=""),
+                radioButtons("color", "Choose a display color for the plot",
+                             choices = c("Green","Blue","Purple"),selected="Purple")
+                
+                        ),
 
         # Show a plot of the generated distribution
-        mainPanel(
-        
-        )
-    )
+            mainPanel(
+                tabsetPanel(type = "tab",
+                tabPanel("Summary Data",verbatimTextOutput("sum")),
+                tabPanel("Species Input Demo",textOutput("specname")),
+                tabPanel("Basic Plot",plotOutput("var")),
+                tabPanel("Complex Plot",plotOutput("var2"))
+                )
+                            
+                    )
+                )
 )
 
 # Define server logic required to draw a histogram
-server <- function(input, output) {
+server <- function(input, output) 
+{
 
-    output$value <- renderPrint({input$action1
-        
-        
-    })
+    colm<-reactive(
+        {
+        as.numeric(input$var)
+        })
+    
+ 
+    output$specname<-renderText(paste("You have selected the species: ",input$species))
+    
+    output$sum<-renderPrint(summary(iris))
+    output$var<-renderPlot(hist(iris[,colm()],col=input$color,main=paste("Histogram of Iris Dataset, All Species by",names(iris[colm()])),xlab=names(iris[colm()])))
+    
+    
+    a<-reactive({
+        iris%>%
+            filter(Species%in%input$species==TRUE)})
+    
+    output$var2<- renderPlot(hist(a()[,colm()],col = input$color,main = paste("Histogram of Iris Dataset by",input$species),xlab = names(a()[colm()])))
+       
+    
 }
 
 # Run the application 
